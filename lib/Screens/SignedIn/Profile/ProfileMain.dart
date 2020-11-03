4a69815ed7dfc4c0a93629388fec2ccb98291907
iamgaurav <gauravs260668@gmail.com>
2020-11-03 11:12:23 +0530

    updated logout,change password

--- lib/Screens/SignedIn/Profile/ProfileMain.dart
+++ lib/Screens/SignedIn/Profile/ProfileMain.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fleva_icons/fleva_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:proapp/Screens/NotSIgnedIn/Login/LoginMain.dart';
 import 'package:proapp/Screens/SignedIn/Profile/changePassword.dart';
 import 'package:proapp/Screens/SignedIn/Profile/editprofile.dart';
 import 'package:proapp/Services/authentication.dart';
@@ -18,8 +23,10 @@ class ProfileMain extends StatefulWidget {
 }
 
 class _ProfileMainState extends State<ProfileMain> {
+  int _currentIndex = 2;
   final String uid;
   _ProfileMainState(this.uid);
+  String _name = "Android", _email = "";
 
   void onSignOut() async {
     try {
@@ -29,6 +36,29 @@ class _ProfileMainState extends State<ProfileMain> {
     } catch (e) {
       print("Error: $e");
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.auth.currentUser().then((user) {
+      setState(() {
+        if (user != null)
+          _email = user.email.toString();
+        else
+          _email = "Loading...";
+      });
+    });
+  }
+
+  Future<bool> signOutUser() async {
+    FirebaseUser user = await auth.currentUser();
+    print(user.providerData[1].providerId);
+    if (user.providerData[1].providerId == 'google.com') {
+      await gooleSignIn.disconnect();
+    }
+    await auth.signOut();
+    return Future.value(true);
   }
 
   @override
@@ -53,50 +83,55 @@ class _ProfileMainState extends State<ProfileMain> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image(image: AssetImage('Assets/img/profilepic.png')),
+                  Image(
+                    image: AssetImage('Assets/img/profilepic.png'),
+                  ),
                   SizedBox(height: 16),
                   Text(
                     "Name here",
-                    style: Heading1(Colors.black),
                   ),
                   SizedBox(height: 16),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Text("ACCOUNT",
-                          style: GoogleFonts.inter(
-                              textStyle: TextStyle(
-                                  fontSize: 10,
-                                  fontWeight: FontWeight.w500,
-                                  color: Color.fromRGBO(0, 0, 0, 0.65)))),
+                      Text(
+                        "ACCOUNT",
+                        style: GoogleFonts.inter(
+                          textStyle: TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.w500,
+                            color: Color.fromRGBO(0, 0, 0, 0.65),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                   SizedBox(height: 9),
                   InkWell(
-                    highlightColor: Colors.transparent,
-                    splashColor: Colors.transparent,
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => EditProfile()),
-                      );
-                    },
-                    child: Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Icon(
-                            FlevaIcons.edit_2,
-                            color: Color(0xFFCBD5E0),
-                          ),
-                          SizedBox(width: 16),
-                          Text(
-                            "Edit Profile",
-                          ),
-                        ],
-                      ),)),
-
+                      highlightColor: Colors.transparent,
+                      splashColor: Colors.transparent,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => EditProfile()),
+                        );
+                      },
+                      child: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              FlevaIcons.edit_2,
+                              color: Color(0xFFCBD5E0),
+                            ),
+                            SizedBox(width: 16),
+                            Text(
+                              "Edit Profile",
+                            ),
+                          ],
+                        ),
+                      )),
                   SizedBox(height: 8),
                   InkWell(
                     highlightColor: Colors.transparent,
@@ -105,7 +140,7 @@ class _ProfileMainState extends State<ProfileMain> {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => changePassword()),
+                            builder: (context) => ChangePassword()),
                       );
                     },
                     child: Container(
@@ -128,15 +163,20 @@ class _ProfileMainState extends State<ProfileMain> {
                     color: Color.fromRGBO(0, 0, 0, 0.25),
                     height: 32,
                   ),
+                  SizedBox(height: 16),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Text("SUPPORT",
-                          style: GoogleFonts.inter(
-                              textStyle: TextStyle(
-                                  fontSize: 10,
-                                  fontWeight: FontWeight.w500,
-                                  color: Color.fromRGBO(0, 0, 0, 0.65)))),
+                      Text(
+                        "SUPPORT",
+                        style: GoogleFonts.inter(
+                          textStyle: TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.w500,
+                            color: Color.fromRGBO(0, 0, 0, 0.65),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                   SizedBox(height: 9),
@@ -185,6 +225,7 @@ class _ProfileMainState extends State<ProfileMain> {
                     color: Color.fromRGBO(0, 0, 0, 0.25),
                     height: 32,
                   ),
+                  SizedBox(height: 16),
                   InkWell(
                     highlightColor: Colors.transparent,
                     splashColor: Colors.transparent,
@@ -193,94 +234,82 @@ class _ProfileMainState extends State<ProfileMain> {
                       showDialog(
                           context: context,
                           builder: (BuildContext context) => Dialog(
-                            insetPadding: EdgeInsets.only(
-                                left: 16,
-                                top: 24,
-                                right: 16,
-                                bottom: 16),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(
-                                    12.0)), //this right here
-                            child: Container(
-                              height: 180.0,
-                              width: 328,
-                              child: Column(
-                                mainAxisAlignment:
-                                MainAxisAlignment.center,
-                                children: <Widget>[
-                                  SizedBox(
-                                    height: 24,
-                                  ),
-                                  Text(
-                                      'Are you sure you want to sign out?',
-                                      style: GoogleFonts.inter(
-                                          textStyle: TextStyle(
-                                              fontSize: 16,
-                                              fontWeight:
-                                              FontWeight.w500))),
-                                  SizedBox(height: 16),
-                                  Container(
-                                    height: 46,
-                                    width: 296,
-                                    child: FlatButton(
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                          BorderRadius.circular(
-                                              6.0),
-                                          side: BorderSide(
-                                              color:
-                                              Color(0xFFFF4128))),
-                                      color: Color(0xFFFF4128),
-                                      textColor: Colors.white,
-                                      child: Text('SIGN OUT',
+                                insetPadding: EdgeInsets.only(
+                                    left: 16, top: 24, right: 16, bottom: 16),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                        12.0)), //this right here
+                                child: Container(
+                                  height: 180.0,
+                                  width: 328,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      SizedBox(
+                                        height: 24,
+                                      ),
+                                      Text('Are you sure you want to sign out?',
                                           style: GoogleFonts.inter(
                                               textStyle: TextStyle(
-                                                  fontFamily:
-                                                  'Intern',
-                                                  fontSize: 14,
+                                                  fontSize: 16,
                                                   fontWeight:
-                                                  FontWeight
-                                                      .w600))),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                        onSignOut();
-                                        Navigator.of(context,
-                                            rootNavigator: true)
-                                            .pop(context);
-                                      },
-                                    ),
+                                                      FontWeight.w500))),
+                                      SizedBox(height: 16),
+                                      Container(
+                                        height: 46,
+                                        width: 296,
+                                        child: FlatButton(
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(6.0),
+                                              side: BorderSide(
+                                                  color: Color(0xFFFF4128))),
+                                          color: Color(0xFFFF4128),
+                                          textColor: Colors.white,
+                                          child: Text('SIGN OUT',
+                                              style: GoogleFonts.inter(
+                                                  textStyle: TextStyle(
+                                                      fontFamily: 'Intern',
+                                                      fontSize: 14,
+                                                      fontWeight:
+                                                          FontWeight.w600))),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                            onSignOut();
+                                            Navigator.of(context,
+                                                    rootNavigator: true)
+                                                .pop(context);
+                                          },
+                                        ),
+                                      ),
+                                      SizedBox(height: 8),
+                                      Container(
+                                        height: 46,
+                                        width: 296,
+                                        child: FlatButton(
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(6.0),
+                                              side: BorderSide(
+                                                  color: Colors.white)),
+                                          color: Colors.white,
+                                          textColor: Color(0xFF718096),
+                                          child: Text('CANCEL',
+                                              style: GoogleFonts.inter(
+                                                  textStyle: TextStyle(
+                                                      fontFamily: 'Intern',
+                                                      fontSize: 14,
+                                                      fontWeight:
+                                                          FontWeight.w600))),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  SizedBox(height: 8),
-                                  Container(
-                                    height: 46,
-                                    width: 296,
-                                    child: FlatButton(
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                          BorderRadius.circular(
-                                              6.0),
-                                          side: BorderSide(
-                                              color: Colors.white)),
-                                      color: Colors.white,
-                                      textColor: Color(0xFF718096),
-                                      child: Text('CANCEL',
-                                          style: GoogleFonts.inter(
-                                              textStyle: TextStyle(
-                                                  fontFamily:
-                                                  'Intern',
-                                                  fontSize: 14,
-                                                  fontWeight:
-                                                  FontWeight
-                                                      .w600))),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ));
+                                ),
+                              ));
                     },
                     child: Container(
                       child: Row(
@@ -291,12 +320,16 @@ class _ProfileMainState extends State<ProfileMain> {
                             color: Color(0xFFCBD5E0),
                           ),
                           SizedBox(width: 16),
-                          Text("Sign Out",
-                              style: GoogleFonts.inter(
-                                  textStyle: TextStyle(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 14,
-                                      color: Color(0xFFFF4128)))),
+                          Text(
+                            "Sign Out",
+                            style: GoogleFonts.inter(
+                              textStyle: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14,
+                                color: Color(0xFFFF4128),
+                              ),
+                            ),
+                          ),
                         ],
                       ),
                     ),
@@ -307,6 +340,32 @@ class _ProfileMainState extends State<ProfileMain> {
           );
         },
       ),
+
+//      bottomNavigationBar: BottomNavigationBar(
+//        currentIndex: _currentIndex,
+//        selectedItemColor: primarygreen,
+//        items: [
+//          BottomNavigationBarItem(
+//            icon: Icon(EvaIcons.star),
+//            title: Text("Feed"),
+//
+//          ),
+//          BottomNavigationBarItem(
+//            icon: Icon(EvaIcons.alertCircleOutline),
+//            title: Text("Complaints"),
+//
+//          ),
+//          BottomNavigationBarItem(
+//            icon: Icon(EvaIcons.person),
+//            title: Text("User Profile"),
+//          )
+//        ],
+//        onTap: (index){
+//          setState(() {
+//            _currentIndex=index;
+//          });
+//        },
+//      ),
     );
   }
 }
